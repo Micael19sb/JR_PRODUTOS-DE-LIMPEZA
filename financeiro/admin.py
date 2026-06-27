b6ec7f8e01e2eee4c9713a3444507d77d341059c
Micael S Barros <micaelcontato09@gmail.com>
2026-06-27 16:55:28 -0300

    corrigir admin.py

--- financeiro/admin.py
+++ financeiro/admin.py
@@ -1,9 +1,21 @@
 from django.contrib import admin
-from .models import ContaReceber
+from .models import MovimentoCaixa, ContaReceber, Despesa, FechamentoCaixa
+
+@admin.register(MovimentoCaixa)
+class MovimentoCaixaAdmin(admin.ModelAdmin):
+    list_display = ('data', 'tipo','categoria','descricao', 'valor')
+    list_filter = ('tipo', 'categoria')
 
 @admin.register(ContaReceber)
 class ContaReceberAdmin(admin.ModelAdmin):
-    list_display = ('cliente', 'valor', 'vencimento', 'pago', 'vencida')
-    list_filter = ('pago', 'vencimento')
-    search_fields = ('cliente__nome',)
-    list_editable = ('pago',)
+    list_display = ('cliente', 'valor_total', 'valor_pago', 'vencimento', 'status')
+    list_filter = ('status',)
+
+@admin.register(Despesa)
+class DespesaAdmin(admin.ModelAdmin):
+    list_display = ('descricao', 'categoria', 'valor', 'data', 'pago')
+    list_filter = ('categoria', 'pago')
+
+@admin.register(FechamentoCaixa)
+class FechamentoCaixaAdmin(admin.ModelAdmin):
+    list_display = ('data', 'total_entradas', 'total_despesas', 'saldo_fechamento', 'fechamento')
